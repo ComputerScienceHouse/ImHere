adb0461decc3f5958b3a2ef4ef9eac465f48d5b4
Nathaniel Aquino <naquino14@outlook.com>
2022-10-10 11:23:38 -0400

    QR Debugging shenanigans

--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Card, CardBody, CardSubtitle, CardTitle } from 'reactstrap'
+import './Attendance.tsx.scss'
+import { QrRenderer } from 'imhere-qgen'
 
 const Attendance = () => {
     return (
-        <div>Attendance</div>
+        <Card>
+            <CardBody>
+                <CardTitle tag='h1'>Generate Attendance QR Code</CardTitle>
+                <CardSubtitle className='mb-2 text-muted' tag='h6'>Select an event below:</CardSubtitle>
+                <p>TODO: add dropdown with events</p>
+                <h3>Qr Debugging:</h3>
+                <QrRenderer squareLength={8} payload={'HelloWorld!'} squares={[]} />
+            </CardBody>
+        </Card>
     )
 }
 
