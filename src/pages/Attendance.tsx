59d1ab06b9e5b8d361414f790104b3363237813b
Nathaniel Aquino <naquino14@outlook.com>
2022-10-17 10:11:30 -0400

    Updated packages

--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Card, CardBody, CardSubtitle, CardTitle } from 'reactstrap'
 import './Attendance.tsx.scss'
-import { QrRenderer } from 'imhere-qgen'
+import { QrRenderer, } from 'imhere-qgen'
+import { GenPayload, GenV4ECIheader } from 'imhere-qgen/lib/encoding'
 
 const Attendance = () => {
+    const spayload = 'deez snuts'
+    const payload = GenPayload(spayload, GenV4ECIheader)
+    console.log('payload', payload)
+
     return (
         <Card>
             <CardBody>
@@ -11,7 +16,7 @@ const Attendance = () => {
                 <CardSubtitle className='mb-2 text-muted' tag='h6'>Select an event below:</CardSubtitle>
                 <p>TODO: add dropdown with events</p>
                 <h3>Qr Debugging:</h3>
-                <QrRenderer squareLength={8} payload={'HelloWorld!'} squares={[]} />
+                <QrRenderer squareLength={10} payload={'HelloWorld!'} />
             </CardBody>
         </Card>
     )
