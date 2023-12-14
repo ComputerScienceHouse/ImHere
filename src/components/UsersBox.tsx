c13481479f40e584f4ecb56b7a2d4bba180b7310
Nate Aquino <naquino14@outlook.com>
2023-12-14 11:54:42 -0500

    Created Usersbox component

---
+++ src/components/UsersBox.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import UserInfo from '../UserInfo'
+import './UsersBox.tsx.scss'
+
+interface Props {
+    users: UserInfo[]
+}
+
+interface UserBoxProps {
+    userInfo: UserInfo
+    key: number
+
+}
+
+const UserBox: React.FC<UserBoxProps> = ({ userInfo, key }) => {
+    const [isHover, setIsHover] = useState(false)
+    const getUserboxPfpStyle = (src: string): React.CSSProperties => {
+        return {
+            width: '40px',
+            height: '40px',
+            marginRight: '5px',
+            backgroundImage: `url(${src}) 0 0 no-repeat fixed`,
+            backgroundSize: 'cover',
+            borderRadius: '50%',
+        }
+    }
+
+    const banUser = () => {
+        console.log(`Banning user ${userInfo.preferred_username}`)
+    }
+
+    return (
+        <div key={key} className='userbox'
+            onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
+            <div className='userbox-pfp' style={getUserboxPfpStyle(`https://profiles.csh.rit.edu/image/${userInfo.preferred_username}`)}
+                onClick={() => banUser()}>
+                <div className={isHover ? 'userbox-pfp_overlay-hover' : 'userbox-pfp_overlay-nohover'}>×</div>
+            </div>
+            <div className='userbox-uname'>
+                {userInfo.preferred_username}
+            </div>
+        </div>
+    )
+}
+
+const UsersBox: React.FC<Props> = ({ users }) => {
+    return (
+        <div className='usersbox-container'>
+            {users.map((userInfo, key) => <UserBox userInfo={userInfo} key={key} />)}
+        </div>
+    )
+}
+
+export default UsersBox
--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -3,6 +3,9 @@ import { Card, CardBody, CardSubtitle, CardText, CardTitle, Input } from 'reacts
 import './Attendance.tsx.scss'
 import { QrRenderer } from 'imhere-qgen'
 import { Request } from '../Fetch'
+import UserInfo from '../UserInfo'
+import { useOidcAccessToken } from '@axa-fr/react-oidc'
+import UsersBox from '../components/UsersBox'
 
 interface Event {
     ID?: number
@@ -15,6 +18,35 @@ const Attendance = () => {
     const [event, setEvent] = useState<Event>({ Name: '' })
     const [qrReady, setQrReady] = useState(false)
     const [errMsg, setErrMsg] = useState('')
+
+    const { accessTokenPayload } = useOidcAccessToken()
+    const userInfo = accessTokenPayload as UserInfo
+
+
+    const testUsers: UserInfo[] = []
+    for (let i = 0; i < 7; i++)
+        testUsers.push(structuredClone(userInfo))
+    // test users
+    testUsers[1].preferred_username = 'samc'
+    testUsers[1].given_name = 'Sam'
+    testUsers[1].family_name = 'Cordry'
+    testUsers[2].preferred_username = 'ethanf108'
+    testUsers[2].given_name = 'Ethan'
+    testUsers[2].family_name = 'Furguson'
+    testUsers[3].preferred_username = 'qel'
+    testUsers[3].given_name = 'Jeremy'
+    testUsers[3].family_name = 'Smart'
+    testUsers[4].preferred_username = 'mattyb'
+    testUsers[4].given_name = 'Mathew'
+    testUsers[4].family_name = 'Breidenbach'
+    testUsers[5].preferred_username = 'mstrodl'
+    testUsers[5].given_name = 'Mary'
+    testUsers[5].family_name = 'Strodl'
+    testUsers[6].preferred_username = 'cinnamon'
+    testUsers[6].given_name = 'Andrew'
+    testUsers[6].family_name = 'Simonson'
+    const [users, setUsers] = useState<UserInfo[]>(testUsers)
+
 
     const getEvent = async () => {
         setErrMsg('')
@@ -37,33 +69,38 @@ const Attendance = () => {
                     <>
                         <CardText tag='h3'>Event {event.ID}: {event?.Name}</CardText>
                         <a href={payload}>Sign In Link</a>
-                        <QrRenderer squareLength={10} payload={payload} />
-                    </> :
-                    <div style={{ width: '100%', height: '60vh' }}>
-                        <CardSubtitle className='mb-2 text-muted' tag='h6'>Add your event name below:</CardSubtitle>
-                        <div className='create-qr'>
-                            <label htmlFor='event-name' className='eventname w-25 lead'>Event Name:</label>
-                            <div className='input-container'>
-                                <Input type='text' value={event?.Name} onChange={e => {
-                                    const text = e.target.value.trim()
-                                    if (text.length === 0) {
-                                        setErrMsg('Event name cannot be empty')
-                                        setEvent({ Name: '' })
-                                        return
-                                    }
-                                    setErrMsg('')
-                                    setEvent({ Name: e.target.value })
-                                }} />
-                                {errMsg ?
-                                    <p className='text-danger'>{errMsg}</p>
-                                    : null
-                                }
-                            </div>
-                            <button className='btn btn-primary btn-sm w-25 ml-5 mt-2 mb-2' onClick={
-                                event.Name.length !== 0 ? getEvent : () => { setErrMsg('Event name cannot be empty') }
-                            }>Create</button>
+                        <div className='d-flex flex-row'>
+                            <QrRenderer squareLength={10} payload={payload} />
+                            <UsersBox users={users} />
                         </div>
-                    </div>
+                    </> :
+                    <>
+                        <div style={{ width: '100%', height: '60vh' }}>
+                            <CardSubtitle className='mb-2 text-muted' tag='h6'>Add your event name below:</CardSubtitle>
+                            <div className='create-qr'>
+                                <label htmlFor='event-name' className='eventname w-25 lead'>Event Name:</label>
+                                <div className='input-container'>
+                                    <Input type='text' value={event?.Name} onChange={e => {
+                                        const text = e.target.value.trim()
+                                        if (text.length === 0) {
+                                            setErrMsg('Event name cannot be empty')
+                                            setEvent({ Name: '' })
+                                            return
+                                        }
+                                        setErrMsg('')
+                                        setEvent({ Name: e.target.value })
+                                    }} />
+                                    {errMsg ?
+                                        <p className='text-danger'>{errMsg}</p>
+                                        : null
+                                    }
+                                </div>
+                                <button className='btn btn-primary btn-sm w-25 ml-5 mt-2 mb-2' onClick={
+                                    event.Name.length !== 0 ? getEvent : () => { setErrMsg('Event name cannot be empty') }
+                                }>Create</button>
+                            </div>
+                        </div>
+                    </>
                 }
             </CardBody>
         </Card>
